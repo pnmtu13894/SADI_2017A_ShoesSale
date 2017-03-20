d2c542a264c5397fc7ba6c72a21cbc5619920a27
dualeoo <dualeoo@gmail.com>
2017-03-20 13:43:30 +0700

    Create the project

---
+++ app/src/main/java/vn/edu/rmit/sadi_2017a_shoessale/MainActivity.java
@@ -0,0 +1,13 @@
+package vn.edu.rmit.sadi_2017a_shoessale;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
